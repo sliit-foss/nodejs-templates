f0a6e3d915c798e6968d9243b0e204d2e8b3e1f3
Akalanka <akalankaperera128@gmail.com>
2023-03-06 12:08:21 +0530

    Updated config to use module logger

--- microservice/src/config/index.js
+++ microservice/src/config/index.js
@@ -1,4 +1,7 @@
 import { Joi } from 'celebrate';
+import { moduleLogger } from '@sliit-foss/module-logger';
+
+const logger = moduleLogger('Config');
 
 class Base {
   static get schema() {
@@ -24,7 +27,7 @@ const config = Base.values;
 const { error } = Joi.object(Base.schema).validate(config);
 
 if (error) {
-  console.error(`Environment validation failed. \nDetails - ${error.details[0].message}\nExiting...`);
+  logger.error(`Environment validation failed. \nDetails - ${error.details[0].message}\nExiting...`);
   process.exit(1);
 }
 
