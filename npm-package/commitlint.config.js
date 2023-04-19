ca63249cb95ab0572a1dbf9390d90d8dc217cccd
MiHi <mrmihiraj@gmail.com>
2023-04-19 14:55:08 +0530

    feature/added npm-package development template

---
+++ npm-package/commitlint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "FEAT!",
+        "FEAT",
+        "FIX",
+        "Feat!",
+        "Feat",
+        "Fix",
+        "Patch",
+        "Chore",
+        "Build",
+        "Refactor",
+        "Revert",
+        "CI",
+        "Test",
+        "Docs",
+        "WIP",
+      ],
+    ],
+    "type-case": [0],
+    "subject-case": [0],
+  },
+};
